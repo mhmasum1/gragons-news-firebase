8d467c3c79dbfdf160fcc9d445cff474249de2ab
mhmasum1 <mhmasum231@gmail.com>
2025-11-04 09:40:05 +0600

    Load & Display News Details Page

---
+++ src/components/NewsDetailsCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const NewsDetailsCard = ({ news }) => {
+    console.log(news)
+    return (
+        <div className='space-y-5'>
+            <img className='object-cover w-full h-[350px]' src={news.image_url} alt="" />
+            <h2 className='text-2xl py-5'>{news.title}</h2>
+            <p>{news.details}</p>
+            <Link className='btn btn-secondary' to={`/category/${news.category_id}`} >Back to Category</Link>
+        </div>
+    );
+};
+
+export default NewsDetailsCard;
